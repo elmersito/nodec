23cc0326fec3bd4e3c9a5eedb464e8167ad492a5
elmersito <101758896+elmersito@users.noreply.github.com>
2022-03-24 19:04:31 -0600

    Update adoptionValidator.js

--- validations/adoptionValidator.js
+++ validations/adoptionValidator.js
@@ -1,34 +1,35 @@
 const { check, validationResult } = require('express-validator');
+const { DATETIME } = require('mysql/lib/protocol/constants/types');
 
 const generateAdoptionValidators = () => [
-    check('user_id').notEmpty().isNumeric().withMessage("Invalid user_id"),
-    check('pet_id').notEmpty().isNumeric().withMessage("Invalid pet_id"),
-    check('date').notEmpty().isDate().withMessage("Invalid date "),
-
+   check('user_id').notEmpty().isLength({max:11}).withMessage("Invalid user_id"),
+   check('pet_id').notEmpty().isLength({max:11}).withMessage("Invalid pet_id"),
+   check('date').notEmpty().isLength(DATETIME).isNumeric().withMessage("Invalid date")
+  
 ]
+
 
 const generateIdValidators = () => [
     check('id').notEmpty().isNumeric().withMessage("Invalid id"),
 ]
-
 const updateAdoptionValidators = () => [
     check('id').notEmpty().isNumeric().withMessage("Invalid id"),
-    check('user_id').notEmpty().isNumeric().withMessage("Invalid user_id"),
-    check('pet_id').notEmpty().isNumeric().withMessage("Invalid pet_id"),
-    check('date').notEmpty().isDate().withMessage("Invalid date "),
-]
+    check('user_id').isLength({max:11}).withMessage("Invalid user_id"),
+    check('pet_id').isLength({max:11}).withMessage("Invalid pet_id")
+ ]
 
 const reporter = (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
+    if(!errors.isEmpty()){
         return res.status(404).json({
-            "success": false,
-            "code": 404,
-            "message": errors,
-            "data": []
+            "success" : false,
+            "code" : 404,
+            "message" : errors,
+            "data" : []
+
         });
     }
-    next();
+  next();  
 }
 
 module.exports = {
@@ -40,8 +41,9 @@ module.exports = {
         generateIdValidators(),
         reporter
     ],
-    update: [
+    update:
+    [
         updateAdoptionValidators(),
         reporter
     ]
-}
+};
