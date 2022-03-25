16ace465c18bd096e9ab31878d4f505b57a39218
elmersito <101758896+elmersito@users.noreply.github.com>
2022-03-24 19:07:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require("express"),
-    app = express(),
-    puerto = process.env.PORT || 1234,
-    mysql = require("mysql"),
-    bodyParser = require("body-parser"),
-    myConnection = require("express-myconnection"),
-    db = require("./database").config;
+const express =require("express"),
+app = express(),
+puerto = process.env.PORT || 3306,
+mysql = require("mysql"),
+bodyParser = require("body-parser"),
+myConnection = require("express-myconnection"),
+db = require("./database").config;
 userRoutes = require("./routes/user");
 petRoutes = require("./routes/pet");
 adoptionRoutes = require("./routes/adoption");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(myConnection(mysql, db));
 app.use("/user", userRoutes);
 app.use("/pet", petRoutes);
 app.use("/adoption", adoptionRoutes);
 
 app.listen(puerto, err => {
-    if (err) {
+    if(err){
         console.log(`Tenemos error ${err}`);
-    } else {
+    }
+    else{
         console.log(`Todo bien en el puerto ${puerto}`);
     }
 })
