0fdbfa0b09e4191754ce00eaaf81436a243637ed
elmersito <101758896+elmersito@users.noreply.github.com>
2022-03-28 16:58:52 -0600

    Update userValidator.js

--- validations/userValidator.js
+++ validations/userValidator.js
@@ -1,50 +1,46 @@
 const { check, validationResult } = require('express-validator');
 
 const generateUserValidators = () => [
-   check('name').notEmpty().isLength({max:50}).withMessage("Invalid name"),
-   check('lastname').notEmpty().isLength({max:50}).withMessage("Invalid lastname"),
-   check('phone').notEmpty().isLength({min:10, max:10}).isNumeric().withMessage("Invalid phone (10 numbers)"),
-   check('address').notEmpty().isLength({max:150}).withMessage("Invalid address")
+    check('name').notEmpty().isLength({ max: 50 }).withMessage('Invalid name'),
+    check('lastname').notEmpty().isLength({ max: 50 }).withMessage('Invalid lastname'),
+    check('phone').optional().notEmpty().isLength({ min: 10, max: 50 }).isNumeric().withMessage('Invalid phone'),
+    check('address').notEmpty().isLength({ max: 150 }).withMessage('Invalid address')
 ]
-
 
 const generateIdValidators = () => [
-    check('id').notEmpty().isNumeric().withMessage("Invalid id"),
+    check('id').notEmpty().isNumeric().withMessage('Invalid ID'),
 ]
-const updateUserValidators = () => [
-    check('id').notEmpty().isNumeric().withMessage("Invalid id"),
-    check('name').isLength({max:50}).withMessage("Invalid name"),
-    check('lastname').isLength({max:50}).withMessage("Invalid lastname"),
-    check('phone').optional().isLength({min:10, max:10}).isNumeric().withMessage("Invalid phone (10 numbers)"),
-    check('address').isLength({max:150}).withMessage("Invalid address")
- ]
 
+const updateUserValidators = () => [
+    check('id').notEmpty().isNumeric().withMessage('Invalid ID'),
+    check('name').notEmpty().isLength({ max: 50 }).withMessage('Invalid name'),
+    check('lastname').notEmpty().isLength({ max: 50 }).withMessage('Invalid lastname'),
+    check('phone').optional().notEmpty().isLength({ min: 10, max: 50 }).isNumeric().withMessage('Invalid phone'),
+    check('address').notEmpty().isLength({ max: 150 }).withMessage('Invalid address')
+]
 const reporter = (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(404).json({
-            "success" : false,
-            "code" : 404,
-            "message" : errors,
-            "data" : []
-
+            "success": false,
+            "code": 404,
+            "message": errors,
+            "data": []
         });
     }
-  next();  
+    next();
 }
-
 module.exports = {
     add: [
         generateUserValidators(),
         reporter
     ],
+    update: [
+        updateUserValidators(),
+        reporter
+    ],
     id: [
         generateIdValidators(),
         reporter
-    ],
-    update:
-    [
-        updateUserValidators(),
-        reporter
     ]
-};
+}
