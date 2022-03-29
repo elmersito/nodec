4295161e07534b6a6501031abc3e060f7d530596
elmersito <101758896+elmersito@users.noreply.github.com>
2022-03-28 18:10:46 -0600

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,13 @@
 let controller = {};
 let format = require("../format").format;
 const jwt = require("jsonwebtoken");
-const db =require("../database").config;
-
-controller.getLogin = (req, res) =>{
-    const user ={
-        id : req.body.id,
-        time : new Date().getTime()
+const db = require("../database").config;
+controller.getLogin = (req, res) => {
+    const user = {
+        id: req.body.id,
+        time: new Date().getTime()
     };
-    const token = jwt.sign({user}, db.secret_key, {expiresIn:'5m'});
+    const token = jwt.sign({ user }, db.secret_key, { expiresIn: '5m' });
     res.status(200);
     format.success = true;
     format.code = 200;
@@ -16,31 +15,24 @@ controller.getLogin = (req, res) =>{
     format.data = token;
     res.json(format);
 };
-controller.getUser = (req, res) =>
-{
+controller.getUser = (req, res) => {
     const sql = "SELECT * FROM users WHERE id = ?";
-	req.getConnection((error,conn) => {
-        if(error)
-        {
+    req.getConnection((error, conn) => {
+        if (error) {
             format.code = 500;
             format.message = "Error to connect to DB, please contact to admin";
             format.success = false;
             res.status(500);
             res.json(format);
-        }
-        else
-        {
-            conn.query(sql, [req.query.id] ,(err, results) =>{
-                if(err)
-                {
+        } else {
+            conn.query(sql, [req.query.id], (err, results) => {
+                if (err) {
                     format.code = 400;
                     format.message = err.sqlMessage;
                     format.success = false;
                     res.status(400);
                     res.json(format);
-                }
-                else
-                {
+                } else {
                     format.code = 200;
                     format.message = "Success";
                     format.success = true;
@@ -48,38 +40,31 @@ controller.getUser = (req, res) =>
                     res.status(200);
                     res.json(format);
                 }
-                
+
             })
         }
-		
-	});
+
+    });
 };
 
-controller.getUsers = (req, res) =>
-{
+controller.getUsers = (req, res) => {
     const sql = "SELECT * FROM users";
-	req.getConnection((error,conn) => {
-        if(error)
-        {
+    req.getConnection((error, conn) => {
+        if (error) {
             format.code = 500;
             format.message = "Error to connect to DB, please contact to admin";
             format.success = false;
             res.status(500);
             res.json(format);
-        }
-        else
-        {
-            conn.query(sql, [req.query.id] ,(err, results) => {
-                if(err)
-                {
+        } else {
+            conn.query(sql, [req.query.id], (err, results) => {
+                if (err) {
                     format.code = 400;
                     format.message = err.sqlMessage;
                     format.success = false;
                     res.status(400);
                     res.json(format);
-                }
-                else
-                {
+                } else {
                     format.code = 200;
                     format.message = "Success";
                     format.success = true;
@@ -87,37 +72,30 @@ controller.getUsers = (req, res) =>
                     res.status(200);
                     res.json(format);
                 }
-                
+
             })
         }
     })
 }
 
-controller.postUser = (req, res) =>
-{
+controller.postUser = (req, res) => {
     const sql = "INSERT INTO users SET ?";
-    req.getConnection((error,conn) => {
-        if(error)
-        {
+    req.getConnection((error, conn) => {
+        if (error) {
             format.code = 500;
             format.message = "Error to connect to DB, please contact to admin";
             format.success = false;
             res.status(500);
             res.json(format);
-        }
-        else
-        {
-            conn.query(sql, [req.body] ,(err, results) => {
-                if(err)
-                {
+        } else {
+            conn.query(sql, [req.body], (err, results) => {
+                if (err) {
                     format.code = 400;
                     format.message = err.sqlMessage;
                     format.success = false;
                     res.status(400);
                     res.json(format);
-                }
-                else
-                {
+                } else {
                     format.code = 201;
                     format.message = "User add";
                     format.success = true;
@@ -125,48 +103,38 @@ controller.postUser = (req, res) =>
                     res.status(201);
                     res.json(format);
                 }
-                
+
             })
         }
     })
 }
 
-controller.putUser = (req, res) =>
-{
+controller.putUser = (req, res) => {
     const sql = "UPDATE users SET ? WHERE id = ?";
-	req.getConnection((error,conn) => {
-        if(error)
-        {
+    req.getConnection((error, conn) => {
+        if (error) {
             format.code = 500;
             format.message = "Error to connect to DB, please contact to admin";
             format.success = false;
             res.status(500);
             res.json(format);
-        }
-        else
-        {
-            conn.query(sql, [req.body, req.body.id] ,(err, results) => {
-                if(err)
-                {
+        } else {
+            conn.query(sql, [req.body, req.body.id], (err, results) => {
+                if (err) {
                     format.code = 400;
                     format.message = err.sqlMessage;
                     format.success = false;
                     res.status(400);
                     res.json(format);
-                }
-                else
-                {
-                    if(results.affectedRows > 0)
-                    {
+                } else {
+                    if (results.affectedRows > 0) {
                         format.code = 200;
                         format.message = "User updated";
                         format.success = true;
                         format.data = results;
                         res.status(200);
                         res.json(format);
-                    }
-                    else
-                    {
+                    } else {
                         format.code = 404;
                         format.message = "User can't be updated, please confirm data";
                         format.success = false;
@@ -174,49 +142,39 @@ controller.putUser = (req, res) =>
                         res.status(404);
                         res.json(format);
                     }
-                    
+
                 }
             })
         }
     })
 }
 
-controller.deleteUser = (req, res) =>
-{
+controller.deleteUser = (req, res) => {
     const sql = "DELETE from users WHERE id = ?";
-    req.getConnection((error,conn) => {
-        if(error)
-        {
+    req.getConnection((error, conn) => {
+        if (error) {
             format.code = 500;
             format.message = "Error to connect to DB, please contact to admin";
             format.success = false;
             res.status(500);
             res.json(format);
-        }
-        else
-        {
-            conn.query(sql, [req.body.id] ,(err, results) => {
-                if(err)
-                {
+        } else {
+            conn.query(sql, [req.body.id], (err, results) => {
+                if (err) {
                     format.code = 400;
                     format.message = err.sqlMessage;
                     format.success = false;
                     res.status(400);
                     res.json(format);
-                }
-                else
-                {
-                    if(results.affectedRows > 0)
-                    {
+                } else {
+                    if (results.affectedRows > 0) {
                         format.code = 204;
                         format.message = "User deleted";
                         format.success = true;
                         format.data = results;
                         res.status(204);
                         res.json(format);
-                    }
-                    else
-                    {
+                    } else {
                         format.code = 404;
                         format.message = "User can't be deleted, please confirm data";
                         format.success = false;
@@ -224,7 +182,7 @@ controller.deleteUser = (req, res) =>
                         res.status(404);
                         res.json(format);
                     }
-                    
+
                 }
             })
         }
