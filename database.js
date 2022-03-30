51b15ca8d57276ac178f331b284710e203ae98c5
elmersito <101758896+elmersito@users.noreply.github.com>
2022-03-29 19:52:22 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-    host: "us-cdbr-east-05.cleardb.net",
-    user: "bee50dcc88dce8",
-    password: "279ef05e",
-    port: 3306,
-    database: "heroku_387d6fe6b649c65",
-    secret_key: "dghssdfh"
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bd7a355ad57b37",
+	password: "431da127",
+	port: 3306,
+	database: "heroku_60b0a2e005c51d8",
+	secret_key: "kj987zhf5643qsf5fd3q"
 };
 exports.config = config;
